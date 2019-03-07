969cb1e8c4a49077b179cdf811d8df3190f85448
MatiPy <matias.pyyhtia@student.saimia.fi>
2019-03-07 13:08:21 +0200

    Added QuestionSystem1

---
+++ QuestionSystem1/QuestionSystem1/Models/student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionSystem1.Models
+{
+    class student
+    {
+        private string etunimi;
+        private string sukunimi;
+
+        public string Etunimi
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public string Sukunimi
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+    }
+}
